634ff47ef31e1647338bd543efaa13ac43c6c757
Dima_V <astrallux777@ukr.net>
2025-12-29 15:37:42 +0200

    29.12.2025-08

---
+++ js/lesson-4.js
@@ -0,0 +1,67 @@
+
+
+// Зробити перемикач теми. Зберігати тему у локальному сховище.
+// При перезавантаженні сторінки перевіряти сховище та ставити тему, яка там вказана.
+// Додати класи для змін тем
+
+const checkbox = document.querySelector(".checkbox");
+
+const body = document.querySelector("body");
+const TOPIC_KEY = "topic";
+
+const darkTheme = "dark-theme";
+const lightTheme = "light-theme";
+
+const getTheme = localStorage.getItem(TOPIC_KEY);
+
+if(getTheme === darkTheme) {
+
+    body.classList.add("dark");
+    checkbox.checked = true;
+};
+
+if(getTheme === lightTheme) {
+
+    body.classList.add("light");
+    checkbox.checked = false;
+};
+
+checkbox.addEventListener("click", handle);
+
+
+
+function handle() {
+
+    if(checkbox.checked === false) {
+localStorage.setItem(TOPIC_KEY, lightTheme);
+body.classList.add("light");
+body.classList.remove("dark");
+    }
+
+if(checkbox.checked === true) {
+localStorage.setItem(TOPIC_KEY, darkTheme);
+body.classList.add("dark");
+body.classList.remove("light");
+    }
+
+}
+
+
+// Створи перелік справ.
+// Є інпут, який вводиться назва завдання.
+// Після натискання на кнопку "Додати" завдання додається до списку #task-list.
+// Поруч із кожним завданням знаходиться кнопка "Видалити", щоб можна було
+// Забрати завдання зі списку.
+// Список із завданнями має бути доступним після перезавантаження сторінки.
+
+<!-- ЗАДАЧА  -->
+
+    <form id="task-form">
+      <input type="text" name="taskName" />
+      <button type="submit">Додати</button>
+    </form>
+
+    <ul id="task-list"></ul>
+
+
+    
